0d2e0886829ab1a29f33b4b42d013993d9056f07
gensan-64 <genyuu6240@gmail.com>
2024-07-24 21:57:22 +0900

    first

---
+++ 20240724/初級研修1.java
@@ -0,0 +1,11 @@
+public  class MyClass {
+    public static void main(String[] args) {
+        // ステップ1: System.out.printlnを使った自己紹介
+        System.out.println("私の名前は山元優輝です。");
+        System.out.println("私は趣味は車の運転ですです。");
+
+        // ステップ2: printfメソッドを使った自己紹介
+        System.out.printf("私の名前は%sです。%n", "[山元]");
+        System.out.printf("私は%sです。%n", "[エンジニア]");
+    }
+}
